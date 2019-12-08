5f8c5bdc9ad6604fbecb5826830d2fa68d238e17
Broday Walker <brodaywalker@gmail.com>
2019-12-08 14:52:14 -0600

    Reorganize folder structure

--- A09/py_scripts/cluster_connect.py
+++ A09/py_scripts/cluster_connect.py
@@ -3,7 +3,7 @@
 from rediscluster import RedisCluster
 
 # Start
-startup_nodes = [{"host": "10.0.88.173", "port": "7000"}]
+startup_nodes = [{"host": "127.0.0.1", "port": "7000"}]
 
 rc = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
 
@@ -11,23 +11,7 @@ rc = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
 if(rc.ping()):
     print("Connection successful...")
 
-# One command test
+
 print(rc.hmset("user:0", {"name": "person", "age": 90}))
-
-# Pipeline test
-# Open pipeline
-p = rc.pipeline()
-
-p.command_stack = []
-
-# Build commands to send
-for i in range(10):
-    p.command_stack.append((["SET", "user:{id}".format(id=i), "person"], {}))
-
-# Send the commands
-p.execute()
-
-
-
 
 
